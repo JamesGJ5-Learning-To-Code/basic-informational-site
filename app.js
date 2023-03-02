63550eb4d4d486133befb167ea6069c7002a2322
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-02 12:09:37 +0000

    Initialise express app

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-const http = require('http');
-const fs = require('fs/promises');
+// const http = require('http');
+// const fs = require('fs/promises');
 
-function chooseFile(path) {
-    switch (path) {
-        case "/":
-            return "index.html";
-        case "/about":
-            return "about.html";
-        case "/contact-me":
-            return "contact-me.html";
-        default:
-            return "404.html";
-    }
-}
+// function chooseFile(path) {
+//     switch (path) {
+//         case "/":
+//             return "index.html";
+//         case "/about":
+//             return "about.html";
+//         case "/contact-me":
+//             return "contact-me.html";
+//         default:
+//             return "404.html";
+//     }
+// }
 
-async function server(req, res) {
-    try {
-        const fileName = chooseFile(req.url);
-        const fileContents = await fs.readFile(fileName);
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(fileContents);
-        return res.end();
-    }
-    catch (err) {
-        console.log(err);
-    }
-}
+// async function server(req, res) {
+//     try {
+//         const fileName = chooseFile(req.url);
+//         const fileContents = await fs.readFile(fileName);
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.write(fileContents);
+//         return res.end();
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
 
-http.createServer(server).listen(8080);
+// http.createServer(server).listen(8080);
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Basic informational website listening on port ${port}!`)
+});
